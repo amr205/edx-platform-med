acfae94e4bbf2fc091c25b377bd545f69164487f
mubbsharanwar <mubbshar.anwar@arbisoft.com>
2023-03-22 09:50:24 +0500

    refactor: Reduce redundant code
    Move recommendations related code to learner_recommendations app.
    
    VAN-1310

--- lms/djangoapps/learner_recommendations/toggles.py
+++ lms/djangoapps/learner_recommendations/toggles.py
@@ -21,6 +21,24 @@ ENABLE_COURSE_ABOUT_PAGE_RECOMMENDATIONS = WaffleFlag(
     f'{WAFFLE_FLAG_NAMESPACE}.enable_course_about_page_recommendations', __name__
 )
 
+# Waffle flag to enable to recommendation panel on learner home mfe
+# .. toggle_name: learner_recommendations.enable_learner_home_amplitude_recommendations
+# .. toggle_implementation: WaffleFlag
+# .. toggle_default: False
+# .. toggle_description: Waffle flag to enable to recommendation panel on learner home mfe
+# .. toggle_use_cases: temporary
+# .. toggle_creation_date: 2023-03-22
+# .. toggle_target_removal_date: None
+# .. toggle_warning: None
+# .. toggle_tickets: VAN-1310
+ENABLE_LEARNER_HOME_AMPLITUDE_RECOMMENDATIONS = WaffleFlag(
+    f"{WAFFLE_FLAG_NAMESPACE}.enable_learner_home_amplitude_recommendations", __name__
+)
+
+
+def enable_learner_home_recommendations():
+    return ENABLE_LEARNER_HOME_AMPLITUDE_RECOMMENDATIONS.is_enabled()
+
 
 def enable_course_about_page_recommendations():
     return ENABLE_COURSE_ABOUT_PAGE_RECOMMENDATIONS.is_enabled()
